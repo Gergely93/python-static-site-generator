4a29dd84e3337a900536bbba55852d9fd879d692
GeriG93 <36864945+GeriG93@users.noreply.github.com>
2022-01-12 18:04:54 +0100

    module5

--- ssg.py
+++ ssg.py
@@ -6,13 +6,13 @@ from ssg.site import Site
 def main(source="content", dest="dist"):
     config = {
         "source": source,
-         "dest": dest,
-         "parsers": [
-             ssg.parsers.ResourceParser(),
-             ssg.parsers.MarkdownParser(),
-             ssg.parsers.ReStructuredTextParser()
-                ],
-              }
+        "dest": dest,
+        "parsers": [
+            ssg.parsers.ResourceParser(),
+            ssg.parsers.MarkdownParser(),
+            ssg.parsers.ReStructuredTextParser(),
+        ],
+    }
     Site(**config).build()
 
 
